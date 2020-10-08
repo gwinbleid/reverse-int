6bef2766fab3976dbf1dca31297c7256a5159832
gwinbleid <corsar909@gmail.com>
2020-10-08 21:59:18 +0300

    feat: solve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let s;
+    if (n > 0) {
+        s = n.toString().split('').reverse().join('');
+    } else {
+        s = (-1 * n).toString().split('').reverse().join('');
+    }
+
+  return +s;
 }
